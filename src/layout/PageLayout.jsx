fd77db79cc44bf26a44b71cfeb6e005ce0544479
Abnnerr <jonnas.ab.00@gmail.com>
2025-06-10 16:54:18 -0300

    updates

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Header from "../components/Header";
 
 const PageLayout = () => {
     return (
         <>
+            <Header />
             <Outlet />
         </>
     );
