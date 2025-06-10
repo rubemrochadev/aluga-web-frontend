ef927a69b8a3538b7657a2a169d7ba2e3e81895c
unknown <conradosousa6730@digitalcollege.internal>
2025-06-10 15:46:07 -0300

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import logo from '../assets/logo.png';
+
+
+
+const Header = () => {
+    return (
+        <header className="py-4 px-[50px] border-b border-[#00000026] ">
+            <nav className="px-2 flex justify-between items-center">
+                <div className="flex items-center cursor-pointer ">
+                    <div className="">
+                        <img src={logo} alt="Logo Aluga Web" />
+
+                    </div>
+
+                </div>
+                <button className="flex items-center cursor-pointer bg-orange-100 text-orange-600 px-4 py-2 rounded-md font-medium hover:bg-orange-200 duration-700">
+                    <box-icon name='user' color='#e65100'></box-icon>
+                    <span>Entrar</span>
+                </button>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
