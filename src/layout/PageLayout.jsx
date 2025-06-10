3806f388e1e630b3c82fa201ddb9411ba16bffbb
unknown <rubempinheiro0184@digitalcollege.internal>
2025-06-10 16:44:32 -0300

    Home

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Header from "../Components/Header";
 
 const PageLayout = () => {
     return (
         <>
+        <Header />
             <Outlet />
         </>
     );
