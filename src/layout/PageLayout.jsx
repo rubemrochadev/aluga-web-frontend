905a8e2a6f8abbed435a970dfe34304e32c9ec0d
DarlisonBrandao <DarlisonBrandao123@gmail.com>
2025-06-11 23:27:39 -0300

    Feat:componente Abas

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Footer from "../components/Footer";
 import Header from "../components/Header"
+import Abas from "../components/Abas";
 
 const PageLayout = () => {
     return (
@@ -8,6 +9,7 @@ const PageLayout = () => {
             <Header />
             <Outlet />
             <Footer />
+            {/* <Abas /> */}
         </>
     );
 }
--- src/pages/Imoveis.jsx
+++ src/pages/Imoveis.jsx
@@ -1,11 +1,14 @@
 
-const Imoveis = () => {
-    return (
-        <div>
-            <h1>Imóveis</h1>
-            <p>Esta é a página de imóveis.</p>
-        </div>
-    );
-}
+// const Imoveis = () => {
+//     return (
+//         <div>
+//             <h1>Imóveis</h1>
+//             <p>Esta é a página de imóveis.</p>
+//         </div>
+//     );
+// }
 
-export default Imoveis;
+// export default Imoveis;
+
+
+
